f382a0790ef7f6e4b4a9e6962cfab9e73e1f2214
Rafał Mierzejewski <rafus.mierzej@interia.pl>
2019-01-26 14:46:36 +0100

    Dodaj pliki projektów.

---
+++ Northwind.Domain/Entities/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Northwind.Domain.Entities
+{
+    class Room
+    {
+        public int RoomId { get; set; }
+        public string Calendar { get; set; }
+        public int Number { get; set; }
+        public int Price { get; set; }
+    }
+}
